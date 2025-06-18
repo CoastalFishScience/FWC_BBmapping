8e9bd3b577e52fa04fb758f28adeef85787655f9
jrode010 <jrode010@fiu.edu>
2025-06-18 18:33:47 -0400

    accuracy assessment on DERM data for 30%, all percentages for our field data

--- Acc_ass_DERM.R
+++ Acc_ass_DERM.R
@@ -308,3 +308,254 @@ head(bb23dat)
 cm2320 <- confusionMatrix(factor(bb23dat20$class), factor(bb23dat20$class1))
 cm23
 cm2320
+
+####30% cover######
+#2016 with treshold at 30% cover
+map1630 <- rast('Maps/S2_BB_SAVmap2016_cover30.tif')
+plot(map1630)
+#have to classify everything at a 2 and above as dense (2)
+bb1630 <- SAVBB %>% dplyr::filter(Year == 2016) %>% mutate(TOT = as.numeric(TOT)) %>% mutate(class1 = if_else(TOT <= 2, 1, 2))
+head(bb1630)
+#make this a spatial object for extraction
+bb16utm30 <- bb1630 %>%
+  st_as_sf(coords = c("easting", "northing"), crs = 32617) 
+
+crs(map1630)
+crs(bb16utm30)
+
+bb16dat30 <- extract(map1630, bb16utm30, bind = T) %>% as.data.frame() 
+head(bb16dat30)
+cm1630 <- confusionMatrix(factor(bb16dat30$class), factor(bb16dat30$class1))
+cm16
+cm1630
+
+#2017 with treshold at 30% cover
+map1730 <- rast('Maps/S2_BB_SAVmap2017_cover30.tif')
+plot(map1730)
+#have to classify everything at a 2 and above as dense (2)
+bb1730 <- SAVBB %>% dplyr::filter(Year == 2017) %>% mutate(TOT = as.numeric(TOT)) %>% mutate(class1 = if_else(TOT <= 2, 1, 2))
+head(bb1730)
+#make this a spatial object for extraction
+bb17utm30 <- bb1730 %>%
+  st_as_sf(coords = c("easting", "northing"), crs = 32617) 
+
+crs(map1730)
+crs(bb17utm30)
+
+bb17dat30 <- extract(map1730, bb17utm30, bind = T) %>% as.data.frame() 
+head(bb17dat30)
+cm1730 <- confusionMatrix(factor(bb17dat30$class), factor(bb17dat30$class1))
+cm17
+cm1730
+
+#2018 with treshold at 30% cover
+map1830 <- rast('Maps/S2_BB_SAVmap2018_cover30.tif')
+plot(map1830)
+#have to classify everything at a 2 and above as dense (2)
+bb1830 <- SAVBB %>% dplyr::filter(Year == 2018) %>% mutate(TOT = as.numeric(TOT)) %>% mutate(class1 = if_else(TOT <= 2, 1, 2))
+head(bb1830)
+#make this a spatial object for extraction
+bb18utm30 <- bb1830 %>%
+  st_as_sf(coords = c("easting", "northing"), crs = 32617) 
+
+crs(map1830)
+crs(bb18utm30)
+
+bb18dat30 <- extract(map1830, bb18utm30, bind = T) %>% as.data.frame() 
+head(bb18dat30)
+cm1830 <- confusionMatrix(factor(bb18dat30$class), factor(bb18dat30$class1))
+cm18
+cm1830
+
+#2019 with treshold at 30% cover
+map1930 <- rast('Maps/S2_BB_SAVmap2019_cover30.tif')
+plot(map1930)
+#have to classify everything at a 2 and above as dense (2)
+bb1930 <- SAVBB %>% dplyr::filter(Year == 2019) %>% mutate(TOT = as.numeric(TOT)) %>% mutate(class1 = if_else(TOT <= 2, 1, 2))
+head(bb1930)
+#make this a spatial object for extraction
+bb19utm30 <- bb1930 %>%
+  st_as_sf(coords = c("easting", "northing"), crs = 32617) 
+
+crs(map1930)
+crs(bb19utm30)
+
+bb19dat30 <- extract(map1930, bb19utm30, bind = T) %>% as.data.frame() 
+head(bb19dat30)
+cm1930 <- confusionMatrix(factor(bb19dat30$class), factor(bb19dat30$class1))
+cm19
+cm1930
+
+#2020 with treshold at 30% cover
+map2030 <- rast('Maps/S2_BB_SAVmap2020_cover30.tif')
+plot(map2030)
+#have to classify everything at a 2 and above as dense (2)
+bb2030 <- SAVBB %>% dplyr::filter(Year == 2020) %>% mutate(TOT = as.numeric(TOT)) %>% mutate(class1 = if_else(TOT <= 2, 1, 2))
+head(bb2030)
+#make this a spatial object for extraction
+bb20utm30 <- bb2030 %>%
+  st_as_sf(coords = c("easting", "northing"), crs = 32617) 
+
+crs(map2030)
+crs(bb20utm30)
+
+bb20dat30 <- extract(map2030, bb20utm30, bind = T) %>% as.data.frame() 
+head(bb20dat30)
+cm2030 <- confusionMatrix(factor(bb20dat30$class), factor(bb20dat30$class1))
+cm20
+cm2030
+
+#2021 with treshold at 30% cover
+map2130 <- rast('Maps/S2_BB_SAVmap2021_cover30.tif')
+plot(map2130)
+#have to classify everything at a 2 and above as dense (2)
+bb2130 <- SAVBB %>% dplyr::filter(Year == 2021) %>% mutate(TOT = as.numeric(TOT)) %>% mutate(class1 = if_else(TOT <= 2, 1, 2))
+head(bb2130)
+#make this a spatial object for extraction
+bb21utm30 <- bb2130 %>%
+  st_as_sf(coords = c("easting", "northing"), crs = 32617) 
+
+crs(map2130)
+crs(bb21utm30)
+
+bb21dat30 <- extract(map2130, bb21utm30, bind = T) %>% as.data.frame() 
+head(bb21dat30)
+cm2130 <- confusionMatrix(factor(bb21dat30$class), factor(bb21dat30$class1))
+cm21
+cm2130
+
+#2022 with treshold at 30% cover
+map2230 <- rast('Maps/S2_BB_SAVmap2022_cover30.tif')
+plot(map2230)
+#have to classify everything at a 2 and above as dense (2)
+bb2230 <- SAVBB %>% dplyr::filter(Year == 2022) %>% mutate(TOT = as.numeric(TOT)) %>% mutate(class1 = if_else(TOT <= 2, 1, 2))
+head(bb2230)
+#make this a spatial object for extraction
+bb22utm30 <- bb2230 %>%
+  st_as_sf(coords = c("easting", "northing"), crs = 32617) 
+
+crs(map2230)
+crs(bb22utm30)
+
+bb22dat30 <- extract(map2230, bb22utm30, bind = T) %>% as.data.frame() 
+head(bb22dat30)
+cm2230 <- confusionMatrix(factor(bb22dat30$class), factor(bb22dat30$class1))
+cm22
+cm2230
+
+#2023 with treshold at 30% cover
+map2330 <- rast('Maps/S2_BB_SAVmap2023_cover30.tif')
+plot(map2330)
+#have to classify everything at a 2 and above as dense (2)
+bb2330 <- SAVBB %>% dplyr::filter(Year == 2023) %>% mutate(TOT = as.numeric(TOT)) %>% mutate(class1 = if_else(TOT <= 2, 1, 2))
+head(bb2330)
+#make this a spatial object for extraction
+bb23utm30 <- bb2330 %>%
+  st_as_sf(coords = c("easting", "northing"), crs = 32617) 
+
+crs(map2330)
+crs(bb23utm30)
+
+bb23dat30 <- extract(map2330, bb23utm30, bind = T) %>% as.data.frame() 
+head(bb23dat30)
+cm2330 <- confusionMatrix(factor(bb23dat30$class), factor(bb23dat30$class1))
+cm23
+cm2330
+
+#Acc ass with current day data
+fielddat <- read.csv('S2_BB_test_set_class.csv')
+fielddat <- fielddat %>% mutate(class10 = if_else(class10 == 'SAV', 2, 1), class20 = if_else(class20 == 'SAV', 2, 1), class30 = if_else(class30 == 'SAV', 2, 1))
+head(fielddat)
+
+##2023 with 10%
+fielddat2310 <- fielddat %>% dplyr::filter(Year == 2023) %>% dplyr::select(TOT, UTM.Easting, UTM.Northing, class10)
+#make this a spatial object for extraction
+fd2310 <- fielddat2310 %>%
+  st_as_sf(coords = c("UTM.Easting", "UTM.Northing"), crs = 32617) 
+fd2310e <- extract(map23, fd2310, bind = T) %>% as.data.frame() 
+cm2310 <- confusionMatrix(factor(fd2310e$class), factor(fd2310e$class10))
+cm2310
+
+##2024 with 10%
+map24 <- rast('Maps/S2_BB_SAVmap2024_cover10.tif')
+fielddat2410 <- fielddat %>% dplyr::filter(Year == 2024) %>% dplyr::select(TOT, UTM.Easting, UTM.Northing, class10)
+#make this a spatial object for extraction
+fd2410 <- fielddat2410 %>%
+  st_as_sf(coords = c("UTM.Easting", "UTM.Northing"), crs = 32617) 
+fd2410e <- extract(map24, fd2410, bind = T) %>% as.data.frame() 
+cm2410 <- confusionMatrix(factor(fd2410e$class), factor(fd2410e$class10))
+cm2410
+
+##2025 with 10%
+map25 <- rast('Maps/S2_BB_SAVmap2025_cover10.tif')
+fielddat2510 <- fielddat %>% dplyr::filter(Year == 2025) %>% dplyr::select(TOT, UTM.Easting, UTM.Northing, class10)
+#make this a spatial object for extraction
+fd2510 <- fielddat2510 %>%
+  st_as_sf(coords = c("UTM.Easting", "UTM.Northing"), crs = 32617) 
+fd2510e <- extract(map25, fd2510, bind = T) %>% as.data.frame() 
+cm2510 <- confusionMatrix(factor(fd2510e$class), factor(fd2510e$class10))
+cm2510
+
+##2023 with 20%
+fielddat2320 <- fielddat %>% dplyr::filter(Year == 2023) %>% dplyr::select(TOT, UTM.Easting, UTM.Northing, class20)
+#make this a spatial object for extraction
+fd2320 <- fielddat2320 %>%
+  st_as_sf(coords = c("UTM.Easting", "UTM.Northing"), crs = 32617) 
+fd2320e <- extract(map2320, fd2320, bind = T) %>% as.data.frame() 
+cm2320 <- confusionMatrix(factor(fd2320e$class), factor(fd2320e$class20))
+cm2320
+
+##2024 with 20%
+map2420 <- rast('Maps/S2_BB_SAVmap2024_cover20.tif')
+fielddat2420 <- fielddat %>% dplyr::filter(Year == 2024) %>% dplyr::select(TOT, UTM.Easting, UTM.Northing, class20)
+#make this a spatial object for extraction
+fd2420 <- fielddat2420 %>%
+  st_as_sf(coords = c("UTM.Easting", "UTM.Northing"), crs = 32617) 
+fd2420e <- extract(map2420, fd2420, bind = T) %>% as.data.frame() 
+cm2420 <- confusionMatrix(factor(fd2420e$class), factor(fd2420e$class20))
+cm2420
+
+##2025 with 20%
+map2520 <- rast('Maps/S2_BB_SAVmap2025_cover20.tif')
+fielddat2520 <- fielddat %>% dplyr::filter(Year == 2025) %>% dplyr::select(TOT, UTM.Easting, UTM.Northing, class20)
+#make this a spatial object for extraction
+fd2520 <- fielddat2520 %>%
+  st_as_sf(coords = c("UTM.Easting", "UTM.Northing"), crs = 32617) 
+fd2520e <- extract(map2520, fd2520, bind = T) %>% as.data.frame() 
+cm2520 <- confusionMatrix(factor(fd2520e$class), factor(fd2520e$class20))
+cm2520
+
+##2023 with 30%
+fielddat2330 <- fielddat %>% dplyr::filter(Year == 2023) %>% dplyr::select(TOT, UTM.Easting, UTM.Northing, class30)
+#make this a spatial object for extraction
+fd2330 <- fielddat2330 %>%
+  st_as_sf(coords = c("UTM.Easting", "UTM.Northing"), crs = 32617) 
+fd2330e <- extract(map2330, fd2330, bind = T) %>% as.data.frame() 
+cm2330 <- confusionMatrix(factor(fd2330e$class), factor(fd2330e$class30))
+cm2310$overall
+cm2320$overall
+cm2330$overall
+
+##2024 with 30%
+map2430 <- rast('Maps/S2_BB_SAVmap2024_cover30.tif')
+fielddat2430 <- fielddat %>% dplyr::filter(Year == 2024) %>% dplyr::select(TOT, UTM.Easting, UTM.Northing, class30)
+#make this a spatial object for extraction
+fd2430 <- fielddat2430 %>%
+  st_as_sf(coords = c("UTM.Easting", "UTM.Northing"), crs = 32617) 
+fd2430e <- extract(map2430, fd2430, bind = T) %>% as.data.frame() 
+cm2430 <- confusionMatrix(factor(fd2430e$class), factor(fd2430e$class30))
+cm2410$overall
+cm2420$overall
+cm2430$overall
+
+##2025 with 30%
+map2530 <- rast('Maps/S2_BB_SAVmap2025_cover30.tif')
+fielddat2530 <- fielddat %>% dplyr::filter(Year == 2025) %>% dplyr::select(TOT, UTM.Easting, UTM.Northing, class30)
+#make this a spatial object for extraction
+fd2530 <- fielddat2530 %>%
+  st_as_sf(coords = c("UTM.Easting", "UTM.Northing"), crs = 32617) 
+fd2530e <- extract(map2530, fd2530, bind = T) %>% as.data.frame() 
+cm2530 <- confusionMatrix(factor(fd2530e$class), factor(fd2530e$class30))
+cm2510$overall
+cm2520$overall
+cm2530$overall
